46b0a0d1ae153ada6d050c89455d3db9f3146333
SirKaelan <kaelanfreakerman6@gmail.com>
2024-11-03 19:03:46 +0200

    Add finite-state machine and conditional rendering

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 
+enum RequestStatus {
+  Loading = "loading",
+  Success = "success",
+  Error = "error",
+}
+
 type Post = {
   userId: number;
   id: number;
@@ -8,24 +14,47 @@ type Post = {
   body: string;
 };
 
+type RequestState =
+  | { status: RequestStatus.Loading }
+  | { status: RequestStatus.Success; data: Post[] }
+  | { status: RequestStatus.Error; errorMsg: string };
+
 function App() {
-  const [posts, setPosts] = useState<Post[]>([]);
+  const [request, setRequest] = useState<RequestState>({
+    status: RequestStatus.Loading,
+  });
 
   useEffect(() => {
-    axios
-      .get<Post[]>("https://jsonplaceholder.typicode.com/posts?_limit=20")
-      .then((response) => {
-        setPosts(response.data);
-      })
-      .catch((error) => {
-        console.log("Errors fetching data:", error);
-      });
+    const fetchPosts = async () => {
+      try {
+        const response = await axios.get<Post[]>(
+          "https://jsonplaceholder.typicode.com/posts"
+        );
+        // Grab latest 20 posts and reverse to show latest post on top
+        const latestPosts = response.data.slice(-20).reverse();
+        setRequest({ status: RequestStatus.Success, data: latestPosts });
+      } catch (err) {
+        if (axios.isAxiosError(err)) {
+          setRequest({ status: RequestStatus.Error, errorMsg: err.message });
+        } else {
+          setRequest({
+            status: RequestStatus.Error,
+            errorMsg: (err as Error).message,
+          });
+        }
+      }
+    };
+
+    fetchPosts();
   }, []);
 
+  if (request.status === RequestStatus.Loading) return <p>Loading...</p>;
+  if (request.status === RequestStatus.Error) return <p>{request.errorMsg}</p>;
+
   return (
-    <div className="p-20 flex flex-col gap-4">
-      {posts.map((post) => (
-        <div key={post.id}>
+    <div className="p-20 flex flex-col gap-4 items-start">
+      {request.data.map((post) => (
+        <div key={post.id} className="p-4 border border-black">
           <p>User id: {post.userId}</p>
           <p>Post id: {post.id}</p>
           <p>Title: {post.title}</p>
