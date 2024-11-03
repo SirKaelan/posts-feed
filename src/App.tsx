5153b31f951e8648be130b48b60ac1409ab9a6a7
SirKaelan <kaelanfreakerman6@gmail.com>
2024-11-03 16:10:55 +0200

    Fetch data from API and display on screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,39 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+type Post = {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+};
+
 function App() {
-  return <div className="text-8xl">Hello, World!</div>;
+  const [posts, setPosts] = useState<Post[]>([]);
+
+  useEffect(() => {
+    axios
+      .get<Post[]>("https://jsonplaceholder.typicode.com/posts?_limit=20")
+      .then((response) => {
+        setPosts(response.data);
+      })
+      .catch((error) => {
+        console.log("Errors fetching data:", error);
+      });
+  }, []);
+
+  return (
+    <div className="p-20 flex flex-col gap-4">
+      {posts.map((post) => (
+        <div key={post.id}>
+          <p>User id: {post.userId}</p>
+          <p>Post id: {post.id}</p>
+          <p>Title: {post.title}</p>
+          <p>Body: {post.body}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default App;
