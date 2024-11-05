aceab9eacd5956c432f52bde9045929a191f1c99
SirKaelan <kaelanfreakerman6@gmail.com>
2024-11-05 12:13:19 +0200

    Add custom form validation

--- src/components/NewPostForm.tsx
+++ src/components/NewPostForm.tsx
@@ -4,13 +4,12 @@ type NewPostFormProps = {
   handleCloseClick: () => void;
 };
 
-type FormValues = {
-  title: string;
-  body: string;
-};
-
 export const NewPostForm = ({ handleCloseClick }: NewPostFormProps) => {
   const [formValues, setFormValues] = useState<FormValues>({
+    title: "",
+    body: "",
+  });
+  const [formErrors, setFormErrors] = useState<FormErrors>({
     title: "",
     body: "",
   });
@@ -20,16 +19,32 @@ export const NewPostForm = ({ handleCloseClick }: NewPostFormProps) => {
   };
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+
     setFormValues((prevState) => ({
       ...prevState,
-      [e.target.name]: e.target.value,
+      [name]: value,
+    }));
+
+    setFormErrors((prevState) => ({
+      ...prevState,
+      [name]: validators[name](e.target.value),
     }));
   };
 
   const handleFormSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    console.log("Form submitted!", formValues);
+    const errors: FormErrors = {
+      title: validators["title"](formValues.title),
+      body: validators["body"](formValues.body),
+    };
+
+    setFormErrors(errors);
+
+    if (Object.values(errors).every((value) => value.length === 0)) {
+      console.log("Form submitted!", formValues);
+    }
   };
 
   return (
@@ -43,24 +58,36 @@ export const NewPostForm = ({ handleCloseClick }: NewPostFormProps) => {
       >
         <h1 className="font-bold">Add New Post</h1>
         <form onSubmit={handleFormSubmit} className="flex flex-col gap-4">
-          <label htmlFor="title-input">Title:</label>
-          <input
-            id="title-input"
-            name="title"
-            type="text"
-            value={formValues.title}
-            onChange={handleInputChange}
-            className="border border-black rounded"
-          />
-          <label htmlFor="body-input">Body:</label>
-          <input
-            id="body-input"
-            name="body"
-            type="text"
-            value={formValues.body}
-            onChange={handleInputChange}
-            className="border border-black rounded"
-          />
+          <div className="flex flex-col">
+            {formErrors.title && (
+              <p className="text-red-600 mb-2">{formErrors.title}</p>
+            )}
+            <label htmlFor="title-input">Title:</label>
+            <input
+              id="title-input"
+              name="title"
+              type="text"
+              value={formValues.title}
+              onChange={handleInputChange}
+              className="border border-black rounded"
+            />
+          </div>
+
+          <div className="flex flex-col">
+            {formErrors.body && (
+              <p className="text-red-600 mb-2">{formErrors.body}</p>
+            )}
+            <label htmlFor="body-input">Body:</label>
+            <input
+              id="body-input"
+              name="body"
+              type="text"
+              value={formValues.body}
+              onChange={handleInputChange}
+              className="border border-black rounded"
+            />
+          </div>
+
           <button type="submit" className="border border-black rounded">
             Submit
           </button>
@@ -68,4 +95,36 @@ export const NewPostForm = ({ handleCloseClick }: NewPostFormProps) => {
       </div>
     </div>
   );
+};
+
+type FormValues = {
+  title: string;
+  body: string;
+};
+
+type FormErrors = {
+  title: string;
+  body: string;
+};
+
+// Form field value validators
+type Validator = (value: string) => string;
+
+const validateTitle: Validator = (title) => {
+  if (title.length === 0) return "Field cannot be empty";
+  return "";
+};
+
+const validateBody: Validator = (body) => {
+  if (body.length === 0) return "Field cannot be empty";
+  return "";
+};
+
+type ValidatorsList = {
+  [key: string]: Validator;
+};
+
+const validators: ValidatorsList = {
+  title: validateTitle,
+  body: validateBody,
 };
