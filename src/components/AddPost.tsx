1623a64c42bbf53e5395df1dc3334c05b2055060
SirKaelan <kaelanfreakerman6@gmail.com>
2024-11-05 11:22:42 +0200

    Add functionality to toggle visibility of form

--- src/components/AddPost.tsx
+++ src/components/AddPost.tsx
@@ -1,7 +1,23 @@
+import { useState } from "react";
+import { NewPostForm } from "./NewPostForm";
+
 export const AddPost = () => {
+  const [showForm, setShowForm] = useState<boolean>(false);
+
+  const handleNewPostClick = () => {
+    setShowForm((prevState) => !prevState);
+  };
+
   return (
-    <button className="border border-black py-1 px-2 rounded hover:bg-blue-600 hover:text-white transition">
-      Add New Post +
-    </button>
+    <div>
+      <button
+        onClick={handleNewPostClick}
+        className="border border-black py-1 px-2 rounded hover:bg-blue-600 hover:text-white transition"
+      >
+        Add New Post +
+      </button>
+
+      {showForm && <NewPostForm />}
+    </div>
   );
 };
---
+++ src/components/NewPostForm.tsx
@@ -0,0 +1,3 @@
+export const NewPostForm = () => {
+  return <div>Form!</div>;
+};
