e1da63844f61a75d50f3c54f8e9ff0bcb990259c
SirKaelan <kaelanfreakerman6@gmail.com>
2024-11-07 00:17:53 +0200

    Clean up unused code

--- src/utils/localStorage.ts
+++ src/utils/localStorage.ts
@@ -11,24 +11,3 @@ export const getFromLocalStorage = <T>(key: string, initialValue: T): T => {
   }
   return JSON.parse(serializedValue) as T;
 };
-
-export const getAllLocalStorageValues = <T>(): T[] => {
-  const values: T[] = [];
-
-  for (let i = 0; i < localStorage.length; i++) {
-    const key = localStorage.key(i);
-    if (key) {
-      try {
-        const value = JSON.parse(localStorage.getItem(key) || "") as T[];
-        values.push(...value);
-      } catch (err) {
-        console.error(
-          `Could not parse value for key ${key} in local storage: `,
-          err
-        );
-      }
-    }
-  }
-
-  return values;
-};
